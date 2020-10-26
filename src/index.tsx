b1fc58e635f4783a8c1a1ed7e4e6a9bb543e7f12
samurai98 <kirito1498@gmail.com>
2020-10-26 22:28:15 +0300

    refactor: fix package.json

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
 import {applyMiddleware, combineReducers, createStore} from 'redux';
 import {chatReducer} from './chat-reducer';
 import thunk from 'redux-thunk';
-import {Provider} from 'react-redux'
+import {Provider} from 'react-redux';
 
 const rootReducer = combineReducers({chat: chatReducer})
 
@@ -26,4 +26,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+serviceWorker.unregister()
